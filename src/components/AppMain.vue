aef972ce8f55e8e86243438237bba4e5dacfd791
Oxidra <dantelezzo@gmail.com>
2023-06-21 12:04:29 +0200

    imported axios & started creating script for api search

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,10 +5,33 @@
 <script>
 import Searchbar from "./Searchbar.vue"
 
+import axios from 'axios';
+
 export default {
     name: "AppMain",
+    data(){
+        return{
+            movieList: [],
+            api: "https://api.themoviedb.org/3/search/movie?api_key=9cf30d55b864819ac35dd0f7928aab85&query="
+        }
+    },
     components: {
         Searchbar
+    },
+
+    methods:{
+        searchMovies(needle = ""){
+            axios.get(this.api, {
+                params:{
+                    name: needle
+                }
+            }).then((response) =>{
+                console.log(response);
+            })
+        }
+    },
+    created(){
+        this.searchMovies();
     }
 }
 </script>
