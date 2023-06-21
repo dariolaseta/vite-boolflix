5c23df7b621799d56722a1ee3d2577c549b35445
Oxidra <dantelezzo@gmail.com>
2023-06-21 11:28:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <AppMain />
+</template>
+
+<script>
+import AppMain from "./components/AppMain.vue"
+
+export default {
+  name: "App",
+  components: {
+    AppMain
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "./styles/general.scss"
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+import Searchbar from "./Searchbar.vue"
+
+export default {
+    name: "AppMain",
+    components: {
+        Searchbar
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    
+</style>
