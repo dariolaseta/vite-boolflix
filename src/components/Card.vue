f83eb9fbdfa0851611d315b67f6594839ec08412
Oxidra <dantelezzo@gmail.com>
2023-06-23 11:15:57 +0200

    created hover effect

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,45 +1,37 @@
 <template>
     <div class="cards">
         <div class="card" v-for="(movies, index) in store.movieList">
-            <ul class="movies">
-                <li >
-                    <div class="d-none">
-                        <p>{{"Titolo: " + store.movieList[index].title}}</p>
-                        <p> {{" Titolo originale: " + store.movieList[index].original_title}}</p>
-                        <p v-if="store.movieList[index].original_language === 'en'"><img src="../assets/imgs/eng.png" alt="" class="lang-img"></p>
-                        <p v-if="store.movieList[index].original_language === 'it'"><img src="../assets/imgs/it.png" alt="" class="lang-img"></p>
-                        <p v-if="store.movieList[index].original_language === 'ja'"><img src="../assets/imgs/jp.png" alt="" class="lang-img"></p>
-                        <p v-if="store.movieList[index].original_language === 'ru'"><img src="../assets/imgs/ru.png" alt="" class="lang-img"></p>
-                        <p v-else>{{ "Lingua: " + store.movieList[index].original_language }}</p>
-                        <p>{{ "Voto: " + numberArray[index] }}</p>
+            <div class="d-none">
+                <p>{{"Titolo: " + store.movieList[index].title}}</p>
+                <p> {{" Titolo originale: " + store.movieList[index].original_title}}</p>
+                <p v-if="store.movieList[index].original_language === 'en'"><img src="../assets/imgs/eng.png" alt="" class="lang-img"></p>
+                <p v-if="store.movieList[index].original_language === 'it'"><img src="../assets/imgs/it.png" alt="" class="lang-img"></p>
+                <p v-if="store.movieList[index].original_language === 'ja'"><img src="../assets/imgs/jp.png" alt="" class="lang-img"></p>
+                <p v-if="store.movieList[index].original_language === 'ru'"><img src="../assets/imgs/ru.png" alt="" class="lang-img"></p>
+                <p v-else>{{ "Lingua: " + store.movieList[index].original_language }}</p>
+                <p>{{ "Voto: " + numberArray[index] }}</p>
     
-                    </div>
-                    <div class="card-img">
-                        <img :src="`https://image.tmdb.org/t/p/w342/${store.movieList[index].poster_path}`" alt="">
-                    </div>
-                </li>
-            </ul>
+            </div>
+            <div class="card-img">
+                <img :src="`https://image.tmdb.org/t/p/w342/${store.movieList[index].poster_path}`" alt="">
+            </div>
         </div>
     
         <div class="card tv" v-for="(series, index) in store.tvSeriesList">
-            <ul class="tv-series">
-                <li>
-                    <div class="d-none">
-                        <p>{{"Titolo: " + store.tvSeriesList[index].name}}</p>
-                        <p> {{" Titolo originale: " + store.tvSeriesList[index].original_name}}</p>
-                        <p v-if="store.tvSeriesList[index].original_language === 'en'"><img src="../assets/imgs/eng.png" alt="" class="lang-img"></p>
-                        <p v-if="store.tvSeriesList[index].original_language === 'it'"><img src="../assets/imgs/it.png" alt="" class="lang-img"></p>
-                        <p v-if="store.tvSeriesList[index].original_language === 'ja'"><img src="../assets/imgs/jp.png" alt="" class="lang-img"></p>
-                        <p v-if="store.tvSeriesList[index].original_language === 'ru'"><img src="../assets/imgs/ru.png" alt="" class="lang-img"></p>
-                        <p v-else>{{ "Lingua: " + store.tvSeriesList[index].original_language }}</p>
-                        <p>{{ "Voto: " + numberArray[index] }}</p>
+            <div class="d-none">
+                <p>{{"Titolo: " + store.tvSeriesList[index].name}}</p>
+                <p> {{" Titolo originale: " + store.tvSeriesList[index].original_name}}</p>
+                <p v-if="store.tvSeriesList[index].original_language === 'en'"><img src="../assets/imgs/eng.png" alt="" class="lang-img"></p>
+                <p v-if="store.tvSeriesList[index].original_language === 'it'"><img src="../assets/imgs/it.png" alt="" class="lang-img"></p>
+                <p v-if="store.tvSeriesList[index].original_language === 'ja'"><img src="../assets/imgs/jp.png" alt="" class="lang-img"></p>
+                <p v-if="store.tvSeriesList[index].original_language === 'ru'"><img src="../assets/imgs/ru.png" alt="" class="lang-img"></p>
+                <p v-else>{{ "Lingua: " + store.tvSeriesList[index].original_language }}</p>
+                <p>{{ "Voto: " + numberArray[index] }}</p>
                         
-                    </div>
-                    <div class="card-img">
-                        <img :src="`https://image.tmdb.org/t/p/w342/${store.tvSeriesList[index].poster_path}`" alt="">
-                    </div>
-                </li>
-            </ul>
+            </div>
+            <div class="card-img">
+                <img :src="`https://image.tmdb.org/t/p/w342/${store.tvSeriesList[index].poster_path}`" alt="">
+            </div>
         </div>
     </div>
 </template>
@@ -70,6 +62,13 @@ export default {
 </script>
 
 <style scoped lang="scss">
+    div.d-none{
+        opacity: 0%;
+    }
+
+    div.d-none:hover{
+        opacity: 100%;
+    }
 
     ul{
         list-style-type: none;
@@ -84,11 +83,11 @@ export default {
         height: 60%;
         width: calc(100% / 5);
     }
-
-    ul{
-        margin: 1rem;
-        li{
             div{
+                p{
+                    margin: 1rem;
+                    width: 100%;
+                }
                 margin: 1rem;
                 img.lang-img{
                     width: 20%;
@@ -96,13 +95,10 @@ export default {
                 div.card-img{
                     margin: 2rem 0;
                     img{
-                        width: 80%;
+                        width: 200px;
                     }
                 }
             }
-        }
-    }
-
     ul.tv-series{
         margin-top: 5rem;
     }
